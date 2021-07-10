2cf666bcbd8664a9e2ed0dfc291b5af60a910cfa
AmandaLimaLuiz <delimaluiz.amanda@gmail.com>
2021-07-10 13:42:12 -0300

    FizzBuzz

---
+++ src/teste/testeFizzBuzz.java
@@ -0,0 +1,31 @@
+package teste;
+import java.util.*;
+public class testeFizzBuzz {
+
+	public static void main(String[] args) {
+		int n;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o valor de n: ");
+		n =  teclado.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			int div3 = i % 3;
+			int div5 = i % 5;
+			 if ((div3 == 0) && (div5 == 0)){
+				 System.out.println("FizzBuzz"); 	
+			 }else if (div3 == 0 ) {
+				 System.out.println("Fizz");
+			 }else if (div5 == 0 ) {
+				 System.out.println("Buzz");
+			 }else {
+				 System.out.println(i);
+			 }
+			 
+		 }
+		
+		
+	teclado.close();	
+	}
+
+}
